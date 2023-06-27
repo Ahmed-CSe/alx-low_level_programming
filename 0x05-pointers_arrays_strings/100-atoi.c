91b2a5ad02d4db7d4ad9e130c55a5a9a5add37ab
Ahmed-CSe <ahmedmeldamhougy@gmail.com>
2023-06-27 09:29:46 -0700

    ksmk

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,12 +10,11 @@
 
 int _atoi(char *s)
 {
-	char *s;
 	unsigned int num = 0;
 	int sign = 1;
 
 	do {
-		if (*s == '-')
+		if (*s = '-')
 			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
 			num = (num * 10) + (*s - '0');
