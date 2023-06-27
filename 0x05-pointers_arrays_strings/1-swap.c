463a2a3252127c131b6e1f08282ecfba80e630b7
Ahmed-CSe <ahmedmeldamhougy@gmail.com>
2023-06-26 19:38:02 -0700

    a

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * sadwadasd
+ * *dawdsa
+ * wadsad
+ * wadasda
+ * dasda: dsad 1
+ * asdd: asds 2
+ *
+ * Return: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b =temp;
+}
