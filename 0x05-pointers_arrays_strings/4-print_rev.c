7c458a8415186c18ab55bf16b6b43564fda18f72
Ahmed-CSe <ahmedmeldamhougy@gmail.com>
2023-06-26 20:14:14 -0700

    ad

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/** print_rev - dasdas das dasd a
+ * @s: sdads asda
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
