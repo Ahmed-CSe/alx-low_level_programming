2c66aee7150da8ccf92f161f0fc5765219c8f49e
Ahmed-CSe <ahmedmeldamhougy@gmail.com>
2023-07-13 17:20:06 -0700

    task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  * main - multiplies two numbers
+ *   * @argc: number of arguments
+ *    * @argv: array of arguments
+ *     *
+ *      * Return: 0 (Success), 1 (Error)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int n = atoi(argv[1]), m = atoi(argv[2]);
+
+	printf("%d\n", n * m);
+	return (0);
+}
