36babaa0c6cc74d89486234f383b48d21dcaa488
Ahmed-CSe <ahmedmeldamhougy@gmail.com>
2023-08-15 05:39:35 +0300

    sd

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define FILE
+
+/*
+ * main - prints the name of the file
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+}
